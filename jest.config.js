d366511645027d6896089863adae320c37b58806
gsiatras <gsiatras@isc.tuc.gr>
2024-10-24 13:02:08 +0300

    feat(devops): added testing

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,22 @@
-const nextJest = require('next/jest');
-
+const nextJest = require('next/jest')
+ 
+/** @type {import('jest').Config} */
 const createJestConfig = nextJest({
-  dir: './', // Specify the root directory of your Next.js app
-});
-
-const customJestConfig = {
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest', // Use babel-jest for JS/TS files
-  },
-  transformIgnorePatterns: [
-    "/node_modules/(?!swiper)" // Transform the swiper module
-  ],
-  testEnvironment: 'jsdom', // Use jsdom for testing
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Setup file for additional configuration
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+ 
+// Add any custom config to be passed to Jest
+const config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  // Add more setup options before each test is run
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Mock CSS imports
-  },
-};
-
-module.exports = createJestConfig(customJestConfig);
+    // ...
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+  }
+}
+ 
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(config)
