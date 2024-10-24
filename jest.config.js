1efb2ad97926bc6e28893fecf9490fb1581fb2fb
gsiatras <gsiatras@isc.tuc.gr>
2024-10-24 12:32:38 +0300

    feat(devops): added testing

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-    testEnvironment: 'jsdom', // Use jsdom for testing React components
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Specify setup file for additional configurations
-    moduleNameMapper: {
-      // Handle CSS imports
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    },
-  };
-  
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    // Handle CSS imports (sass, less, etc.)
+    '\\.(css|less|scss)$': 'identity-obj-proxy',
+    // Handle image imports
+    '\\.(png|jpg|jpeg|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'], // Specify the setup file
+  transform: {
+    // Use babel-jest to transpile tests
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'], // Ignore Next.js build directory
+};
